089de8232697795514de885f279bfcd298d98b46
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-09-26 16:19:42 +0200

    add some directive attributes and structural directives for style

--- src/app/article-component/article-component.component.ts
+++ src/app/article-component/article-component.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 interface Article {
+  id: number;
   title: string;
   author: string;
   description: string;
@@ -10,6 +11,8 @@ interface Article {
   image: string;
   isPublished: boolean;
   comment: string;
+  likes: number;
+  isLiked: boolean;
 }
 
 @Component({
@@ -22,50 +25,78 @@ interface Article {
 export class ArticleComponentComponent {
   article: Article[] = [
     {
+      id: 0,
       title: 'Album Review | Sabrina Carpenter',
       author: 'Emilie',
       description: "Short n' Sweet",
       content:
-        'Slayin, stunnin, yesssss. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh.',
+        'Slayin, stunnin, yesssss. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
       image: 'https://i.scdn.co/image/ab6761610000e5ebe053b8338322b9c8609ee7ae',
       isPublished: true,
       comment: '',
+      likes: 150,
+      isLiked: false,
     },
     {
+      id: 1,
       title: 'Album Review | Chappell Roan',
       author: 'Emilie',
       description: 'The Rise and Fall of a Midwest Princess',
       content:
-        'Slayin, stunnin, yesssss. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh.',
+        'Slayin, stunnin, yesssss. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
       image:
         'https://upload.wikimedia.org/wikipedia/en/3/34/Chappell_Roan_-_The_Rise_and_Fall_of_a_Midwest_Princess.png',
       isPublished: true,
       comment: '',
+      likes: 180,
+      isLiked: false,
     },
     {
+      id: 2,
       title: 'Album Review | Charlie XCX',
       author: 'Emilie',
       description: 'Brat',
       content:
-        'Slayin, stunnin, yesssss. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh.',
+        'Slayin, stunnin, yesssss. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
       image: 'https://img.rasset.ie/00209253-614.jpg?ratio=1',
       isPublished: true,
       comment: '',
+      likes: 80,
+      isLiked: false,
     },
     {
+      id: 3,
       title: 'Album Review | Billie Eilish',
       author: 'Emilie',
       description: 'Hit Me Hard and Soft',
       content:
-        'Slayin, stunnin, yesssss. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh.',
+        'Slayin, stunnin, yesssss. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
       image:
         'https://www.francetvinfo.fr/pictures/UI5V0DUM90Bj7Vv1AxS1-VendVQ/1200x1200/2024/05/16/hitmehardandsoft-6645f287dba62732570309.jpg',
       isPublished: true,
       comment: '',
+      likes: 120,
+      isLiked: false,
     },
   ];
 
   togglePublication(index: number): void {
     this.article[index].isPublished = !this.article[index].isPublished;
+  }
+
+  addLike(index: number): void {
+    this.article[index].likes += 1;
+    this.article[index].isLiked = true;
+  }
+  // style à appliquer à la carte si elle est publiée ou non
+  getCard(isPublished: boolean) {
+    return {
+      'background-color': 'rgb(178, 221, 203)',
+      'border-bottom': '10px solid #fe85fa',
+      'border-right': '3px solid #fe85fa',
+      'max-height': isPublished ? '1000px' : '0',
+      opacity: isPublished ? '1' : '0',
+      overflow: 'hidden',
+    };
   }
 }
