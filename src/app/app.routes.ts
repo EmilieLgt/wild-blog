603dee14226c546b6d544ee319fc4ff2ff8337d5
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-09-30 21:30:11 +0200

    add a reactive form for sign-up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ArticleComponentComponent } from './article-component/article-component.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 export const routes: Routes = [
   {
@@ -16,6 +17,10 @@ export const routes: Routes = [
   {
     path: 'contact',
     component: ContactPageComponent,
+  },
+  {
+    path: 'sign-up',
+    component: RegisterPageComponent,
   },
   {
     path: '**',
