c60e2c4821dffafb23d40572bf802c778adca878
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-09-30 16:12:35 +0200

    add contact page and form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ArticleComponentComponent } from './article-component/article-component.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'article/:id',
     component: ArticleComponentComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent,
   },
   {
     path: '**',
