b281462f4e9279a04e019213620ac5e40dec6aec
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-11-13 14:59:42 +0100

    add a service for api calls

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,10 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Article } from '../models/Article';
 import { ArticleComponent } from '../articles/articles.component';
-import { RouterModule } from '@angular/router';
 import { OneArticleComponent } from '../one-article/one-article.component';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../services/api.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
@@ -15,24 +13,26 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     FormsModule,
     CommonModule,
-    RouterModule,
     ArticleComponent,
     OneArticleComponent,
     HttpClientModule,
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
+  providers: [ApiService],
 })
 export class HomePageComponent {
-  articles$!: Observable<Article[]>;
+  articles: Article[] = [];
+  private apiService = inject(ApiService);
 
-  http = inject(HttpClient);
+  articleLikedtoDisplay!: string;
 
   ngOnInit() {
-    this.articles$ = this.http.get<Article[]>('http://localhost:3000/articles');
+    this.apiService.getArticles().subscribe((articles) => {
+      this.articles = articles;
+    });
   }
   // on reprend ici les informations de l'enfant pour pouvoir afficher le titre d'un article liké
-  articleLikedtoDisplay!: string;
   handleArticleToDisplay(articleTitle: string) {
     this.articleLikedtoDisplay = articleTitle;
   }
